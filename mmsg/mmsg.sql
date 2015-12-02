10c6614a56cf45e31ec0aff5e0990161459531af
sujinw <sujinw@qq.com>
2015-12-02 15:07:37 +0800

    thinkPHP学习作品
    thinkPHP学习作品

---
+++ mmsg/mmsg.sql
@@ -0,0 +1,68 @@
+﻿# Host: localhost  (Version: 5.5.38)
+# Date: 2015-07-23 22:42:56
+# Generator: MySQL-Front 5.3  (Build 4.120)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "msgs"
+#
+
+DROP TABLE IF EXISTS `msgs`;
+CREATE TABLE `msgs` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '留言标题',
+  `body` text COMMENT '留言内容',
+  `userid` int(11) NOT NULL DEFAULT '0',
+  `time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "msgs"
+#
+
+/*!40000 ALTER TABLE `msgs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `msgs` ENABLE KEYS */;
+
+#
+# Structure for table "rmsgs"
+#
+
+DROP TABLE IF EXISTS `rmsgs`;
+CREATE TABLE `rmsgs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `msgid` int(11) NOT NULL,
+  `body` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "rmsgs"
+#
+
+/*!40000 ALTER TABLE `rmsgs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rmsgs` ENABLE KEYS */;
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `logintime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "users"
+#
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
